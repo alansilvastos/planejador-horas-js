bdf3304d70325a5cc9de4dd2e859cda7f6d16c1c
alansilvastos <alansilvastos@gmail.com>
2025-06-21 22:47:19 -0300

    Update button.jsx

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -1,1 +1,10 @@
-export function Button({ children }) { return <button>{children}</button>; }
+export function Button({ children, ...props }) {
+  return (
+    <button
+      {...props}
+      className="bg-blue-600 text-white px-4 py-1 rounded hover:bg-blue-700 transition"
+    >
+      {children}
+    </button>
+  );
+}
