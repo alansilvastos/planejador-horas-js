63826273e4ce8ab5adc9e848b1a2d0e73ba635a0
alansilvastos <alansilvastos@gmail.com>
2025-06-23 14:16:30 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,23 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '1rem'
-        }}>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-poppins bg-white text-black">
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '1rem'
+          }}
+        >
           {children}
         </div>
       </body>
