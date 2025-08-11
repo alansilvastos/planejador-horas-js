68241655b1f421c7a1ad54374840ba1f29183eba
alansilvastos <alansilvastos@gmail.com>
2025-08-11 10:35:47 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
